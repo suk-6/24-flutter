0e204c15104a2e2080c1f3d227a994bbd8e3a0d6
suk-6 <me@suk.kr>
2024-05-28 00:02:52 +0900

    init

---
+++ flutter_base/android/app/src/main/kotlin/kr/suk/flutter_base/MainActivity.kt
@@ -0,0 +1,5 @@
+package kr.suk.flutter_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_base/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Text(
+              'id',
+              style: TextStyle(
+                fontSize: 10,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your id',
+              ),
+            ),
+          ],
+        )
+      ],
+    )));
+  }
+}
